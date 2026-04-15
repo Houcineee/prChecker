d6749753b14b0308beba56c3b073abdfa19c8374
Houcinee <lahoucinegahboub@gmail.com>
2026-04-15 13:05:47 +0000

    add: for testing

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.post("/" , (req, res)=>{
   console.log(req.body)
   res.status(200)
-  res.send("<h1> all good <h1>")
+  res.send("<h1> For testing purposes <h1>")
 }) ;
 const PORT = 8080
 app.listen(PORT , ()=>{
